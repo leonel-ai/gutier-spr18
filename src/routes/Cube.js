941eb25e71ba18eaf1fc020f0b406c87f64ec0dd
Leonela Guzman <leonel-ai@users.noreply.github.com>
2018-01-06 12:43:15 -0600

    latest

--- src/routes/Cube.js
+++ src/routes/Cube.js
@@ -6,6 +6,21 @@ import '../styles/Main.css';
 class Cube extends Component {
     constructor(props) {
         super(props);
+
+        this.onClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        if (document.getElementsByClassName('front')) {
+            var front = document.getElementsByClassName('front');
+            console.log('does match');
+        } else {
+            console.log('does not match');
+        }
+        // document.getElementsByClassName('front').classList.toggle('turnPage');
+        // document.getElementsByClassName('back').classList.toggle('turnPage');
+        // document.getElementsByClassName('right').classList.toggle('turnPage');
+        // document.getElementsByClassName('left').classList.toggle('turnPage');
     }
 
     render() {
@@ -13,22 +28,22 @@ class Cube extends Component {
             <div className="mainDiv">
                 <div className="cube">
 
-                    <div className="front">
+                    <div className="front" onClick={this.handleClick} >
                         <p><span className="hm-largeTxt">INNOVATION</span><br/>starts from the top</p>
                         <button className="hm-button"><Link to="/services">Services</Link></button>
                     </div>
 
-                    <div className="back">
+                    <div className="back" onClick={this.handleClick} >
                         <p><span className="hm-largeTxt">EXCELLENCE</span><br/>starts from the top</p>
                         <button className="hm-button"><Link to="/news">News</Link></button>                        
                     </div>
 
-                    <div className="right">
+                    <div className="right" onClick={this.handleClick} >
                         <p><span className="hm-largeTxt">COMMITMENT</span><br/>starts from the top</p>
                         <button className="hm-button"><Link to="/contact">Contact</Link></button>                         
                     </div>
 
-                    <div className="left">
+                    <div className="left" onClick={this.handleClick} >
                         <p><span className="hm-largeTxt">INTEGRITY</span><br/>starts from the top</p>
                         <button className="hm-button"><Link to="/about">About Us</Link></button> 
                     </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,54 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Cube from './Cube';
-import WheelReact from 'wheel-react';
+// import WheelReact from 'wheel-react';
 import '../styles/Main.css';
 
 class Home extends Component {
     constructor(props) {
         super(props);
-        // this.onClick = this.handleClick.bind(this);
-
+        
         this.state = {
             content: <Cube />
         };
-        WheelReact.config({
-            left: () => {
-                this.setState({
-                    content: 'left direction detected.'
-                });
-            },
-            right: () => {
-                this.setState({
-                    content: 'right direction detected.'
-                });
-            },
-            up: () => {
-                this.setState({
-                    content: 'up direction detected.'
-                });
-            },
-            down: () => {
-                this.setState({
-                    content: 'down direction detected.'
-                });
-            }
-        });
+    //     WheelReact.config({
+    //         left: () => {
+    //             this.setState({
+    //                 content: 'left direction detected.'
+    //             });
+    //         },
+    //         right: () => {
+    //             this.setState({
+    //                 content: 'right direction detected.'
+    //             });
+    //         },
+    //         up: () => {
+    //             this.setState({
+    //                 content: 'up direction detected.'
+    //             });
+    //         },
+    //         down: () => {
+    //             this.setState({
+    //                 content: 'down direction detected.'
+    //             });
+    //         }
+    //     });
     }
 
     componentWillMount() {
         this.props.history.push('/');
     }
 
-    componentWillUnmount() {
-        WheelReact.clearTimeout();
-    }
+    // componentWillUnmount() {
+    //     WheelReact.clearTimeout();
+    // }
 
 
     render() {
 
         return (
-            <div {...WheelReact.events} tabIndex="1" className="hm-grid">
+            // <div {...WheelReact.events} tabIndex="1" className="hm-grid">
+            <div className="hm-grid">
                 {this.state.content}
             </div>
         )
