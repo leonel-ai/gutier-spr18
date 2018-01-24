4f6be000e6c0d84bf7f707903877a7005366bc20
Leonela Guzman <leonel-ai@users.noreply.github.com>
2018-01-23 18:32:13 -0600

    home page aframe bug fixed

--- src/routes/VrCube.js
+++ src/routes/VrCube.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
 import '../styles/Routes.css';
 
 import 'aframe';
-import 'aframe-animation-component';
-import 'aframe-particle-system-component';
-import 'babel-polyfill';
-import {Entity, Scene} from 'aframe-react';
+// import 'aframe-animation-component';
+// import 'aframe-particle-system-component';
+// import 'babel-polyfill';
+import {Scene} from 'aframe-react';
 
 import panImg from '../assets/360.jpg';
 
@@ -14,15 +15,18 @@ class Cube extends Component {
 
   render () {
     return (
+      <div className="vr-homepage">
+        <Helmet>
+          <meta charSet="utf-8" />
+            <title>Gutier Roofing</title>
+            <script src="../../../dist/aframe-master.js"></script>
+        </Helmet>
+
       <Scene>
           <a-sky id="image-360" radius="20" src={panImg} rotation="0 -130 0"></a-sky>
           <a-camera wasd-controls-enabled="false"></a-camera>
-
-        <Entity primitive="a-light" type="ambient" color="#445451"/>
-        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-
       </Scene>
+      </div>
     );
   }
 }
